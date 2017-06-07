6a91895ba2e2fdf0d160715af35300b8f7db1d93
WonJi Ko <kwj9211@gmail.com>
2017-06-08 01:19:01 +0900

    git initialize

---
+++ src/main/java/edu/cheecho/wikitriple/server/Application.java
@@ -0,0 +1,13 @@
+package edu.cheecho.wikitriple.server;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Cheechyo on 2017-06-08.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
