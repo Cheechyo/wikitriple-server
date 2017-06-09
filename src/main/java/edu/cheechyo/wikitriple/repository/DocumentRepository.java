874c01225d714ac3da95f625cab7a4a8f008082b
Cheechyo <kwj9211@gmail.com>
2017-06-09 18:04:22 +0900

    version을 적용한 edit과 read

--- src/main/java/edu/cheechyo/wikitriple/repository/DocumentRepository.java
+++ src/main/java/edu/cheechyo/wikitriple/repository/DocumentRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.repository.CrudRepository;
  * Created by Cheechyo on 2017. 6. 8..
  */
 public interface DocumentRepository extends CrudRepository<Document,Integer> {
+    Document findTopByTitleOrderByVersionDesc(String title);
 
+    int countByTitle(String title);
 }
